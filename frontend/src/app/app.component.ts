92c1ffb0303703780ebd6e069f86cac0f385d3d5
Canto <acanto95@bitbucket.org>
2018-05-08 18:09:44 -0500

    actualizacion del frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,25 +1,41 @@
-    import { Component } from '@angular/core';
-    import { Http, Response } from '@angular/http';
-    import { Observable } from 'rxjs';
-    import 'rxjs/add/operator/toPromise';
+import {Component} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-    @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+ 
+  title = 'app works!';
+  private apiUrl = 'http://localhost:8000/viajes/';
+  data: any ={};
+
+  constructor(private http: Http){
+    console.log('Hello Fellow user');
+    this.getViajes();
+    this.getData();
+
+  }
+
+
+  getData(){
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json())
+  }
+
+  getViajes(){
+    this.getData().subscribe(data =>{
+      console.log(data);
+      this.data = data
     })
-    export class AppComponent {
-    title = 'app';
-    url : string = 'http://localhost:8000/viajes/1';
+  }
 
 
-    constructor(private http : Http){}
-    public getViajes(){
+}
 
-        this.http.get(this.url).toPromise().then((res)=>{
-            console.log(res.json());
-        });
-
-    }
-    }
 
---
+++ frontend/src/app/demo.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+ 
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+ 
+@Injectable()
+export class DemoService {
+ 
+    constructor(private http:HttpClient) {}
+     getViajes() {
+        return this.http.get('localhost:8000/viajes/');
+    }
+}
+
